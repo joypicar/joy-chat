819c04f6e7174163806e2f65a0a66bc552d60ceb
joypicar <joysancute@gmail.com>
2023-01-30 19:02:26 +0800

    changed another

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
     io = require('socket.io').listen(server),
     users = {};
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public/'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
